6a1df65886f96c74f3b12a4e6b8fc5535fa23acc
tinycarrot <49933370+tinycarrot@users.noreply.github.com>
2019-10-23 01:02:35 +0700

    Update vip.beautyplus.js

--- quantumutlx/script/vip.beautyplus.js
+++ quantumutlx/script/vip.beautyplus.js
@@ -7,7 +7,7 @@ let obj = JSON.parse(body);
 
 if (url.indexOf(path1) != -1) {
 	obj["subs_status"] = 2;
-	obj["vip_product_id"] = com.commsource.beautyplus.subscription.1year.25discount.new.users.1205testA;
+	obj["vip_product_id"] = "com.commsource.beautyplus.subscription.1year.25discount.new.users.1205testA";
 	obj["vip_expires_date_ms"] = 4084430261000;
 	
 	body = JSON.stringify(obj);  
