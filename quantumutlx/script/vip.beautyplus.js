63a8c4f59522b6c14435947accd3c85f8871c658
tinycarrot <49933370+tinycarrot@users.noreply.github.com>
2019-10-23 01:11:47 +0700

    Update vip.beautyplus.js

--- quantumutlx/script/vip.beautyplus.js
+++ quantumutlx/script/vip.beautyplus.js
@@ -6,9 +6,9 @@ const path1 = '/v1/subs_offer_elg';
 let obj = JSON.parse(body);
 
 if (url.indexOf(path1) != -1) {
-	obj["subs_status"] = 2;
-	obj["vip_product_id"] = "com.commsource.beautyplus.subscription.1year.25discount.new.users.1205testA";
-	obj["vip_expires_date_ms"] = 4084430261000;
+	obj.subs_status = 2;
+	obj.vip_product_id = "com.commsource.beautyplus.subscription.1year.25discount.new.users.1205testA";
+	obj.vip_expires_date_ms = 4084430261000;
 	
 	body = JSON.stringify(obj);  
  }
