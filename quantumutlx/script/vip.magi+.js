8c6163832bba4d32602e7b8ff04f367d0b6b6607
tinycarrot <49933370+tinycarrot@users.noreply.github.com>
2019-10-22 17:19:31 +0700

    Update vip.magi+.js

--- quantumutlx/script/vip.magi+.js
+++ quantumutlx/script/vip.magi+.js
@@ -1,23 +1,30 @@
 
 var obj = JSON.parse($response.body);
-if(obj.receipt.adam_id("1454351172")){
+let id = obj.receipt['adam_id'];
+if(id == 1454351172){
         obj = {
-          "user_subscription": {
-            "user_id": 20167004,
-            "subscription_code": "VSCOANNUAL",
-            "sku": "VSCOANNUAL",
-            "expired": false,
-            "starts_on_sec": null,
-            "expires_on_sec": null,
-            "last_verified_sec": 1566463819,
-            "canceled_at_sec": null,
-            "source": 1,
-            "payment_type": 4,
-            "invalid_reason": 2,
-            "is_active": true,
-            "is_in_grace_period": false
-          },
-          "created_at": "2019-10-21T21:22:39.719371376Z"
+          	"receipt": {
+		"receipt_type": "Production",
+		"adam_id": 1454351172,
+		"app_item_id": 1454351172,
+		"bundle_id": "com.video.magicam",
+		"application_version": "77",
+		"download_id": 32064290042342,
+		"version_external_identifier": 832981086,
+		"receipt_creation_date": "2019-10-22 09:45:27 Etc/GMT",
+		"receipt_creation_date_ms": "1571737527000",
+		"receipt_creation_date_pst": "2019-10-22 02:45:27 America/Los_Angeles",
+		"request_date": "2019-10-22 09:47:59 Etc/GMT",
+		"request_date_ms": "1571737679284",
+		"request_date_pst": "2019-10-22 02:47:59 America/Los_Angeles",
+		"original_purchase_date": "2019-10-22 09:45:27 Etc/GMT",
+		"original_purchase_date_ms": "1571737527000",
+		"original_purchase_date_pst": "2019-10-22 02:45:27 America/Los_Angeles",
+		"original_application_version": "77",
+		"in_app": []
+	},
+	"status": 0,
+	"environment": "Production"
         };
 };
 $done({body: JSON.stringify(obj)});
