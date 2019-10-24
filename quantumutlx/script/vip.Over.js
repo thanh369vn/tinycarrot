c86714515b6517292abd0a8eefe90d9abeaabfd7
tinycarrot <49933370+tinycarrot@users.noreply.github.com>
2019-10-25 00:16:43 +0700

    Update vip.Over.js

--- quantumutlx/script/vip.Over.js
+++ quantumutlx/script/vip.Over.js
@@ -1,8 +1,8 @@
 var body = $response.body;
 let obj = JSON.parse(body);
 
-	obj.user.subscription["isSubscriptionActive"] = false;
-	obj.user.subscription["entitlement"] = ["OVER_PRO2"];
+	obj.user.subscription["isSubscriptionActive"] = true;
+	obj.user.subscription["entitlement"] = ["OVER_PRO"];
 	body = JSON.stringify(obj);
 
 $done({body});
